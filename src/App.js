a209c8a09a385213cc8001a479bbe0a563ce24b2
IamShinuRaju <rajushinu007@gmail.com>
2021-01-02 18:30:56 +0530

    myFirstCommit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+
 
 function App() {
+    const [Number, setNumber] = useState(0);
+
+const addOne =() => {
+  return setNumber(Number+1)
+}
+
+const minusOne = () => {
+  return setNumber(Number - 1);
+};
+ 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span onClick={minusOne}> minus 1 </span>
+        <span>{Number}</span>
+        <span onClick={addOne}> add 1 </span>
       </header>
     </div>
   );
